108a97068c42b5ea079d77d567bb251dbace6182
Salvatore Allegra <salallegra@gmail.com>
2020-03-20 05:54:11 -0700

    Add new template files and add profile pic, adjust margin and image size

--- app.js
+++
@@ -1,8 +0,0 @@
-function closePreNav() {
-  var x = document.getElementById("pre-nav");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
