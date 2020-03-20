3c6c5f96efbd7a2dfcf47dab4afe6f9191ba612e
Salvatore Allegra <salallegra@gmail.com>
2020-03-20 09:38:49 -0700

    Add port for environment so it will run on Heroku

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const path = require("path");
 const express = require("express");
 const router = express.Router();
 const app = express();
-
+const port = process.env.PORT || 3000;
 // app.get("/", function(req, res) {
 //   res.sendFile(path.join(__dirname + "/index.html"));
 // });
 
 app.use(express.static("public"));
-app.listen(3000);
+app.listen(port);
