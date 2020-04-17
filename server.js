7ad1fb915ae51405ffb770eaf2e65d7160808770
Salvatore Allegra <salallegra@gmail.com>
2020-04-16 19:20:40 -0700

    Delete everything

--- server.js
+++
@@ -1,7 +0,0 @@
-const path = require("path");
-const express = require("express");
-const router = express.Router();
-const app = express();
-const port = process.env.PORT || 3000;
-app.use(express.static("public"));
-app.listen(port);
